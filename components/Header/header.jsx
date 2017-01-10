6352ab8cccfe70bcb7b0e88974617656367c49be
jirishus <jirishus@yahoo.com>
2017-01-10 12:00:39 -0800

    Bootstrap setup + Generic Header

---
+++ components/Header/header.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+
+class Header extends React.Component {
+
+ constructor(props) {
+  super(props);
+ }
+
+ render() {
+  return (
+    <header className="mainHeader">
+     <p>Generic Header</p>
+    </header>
+   )
+ }
+
+}
+
+export default Header;
